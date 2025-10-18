44fd733a90cbe767c759562732ca6eda20e3106c
wallclimbr <wallclimbr@mailbox.org>
2025-10-18 19:54:26 +0200

    fixed FHE deploy script

--- packages/fhevm-hardhat-template/deploy/deploy.ts
+++ packages/fhevm-hardhat-template/deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedFHEDataCollector = await deploy("FHEDataCollector", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`FHEDataCollector contract: `, deployedFHEDataCollector.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_fheDataCollector"; // id required to prevent reexecution
+func.tags = ["FHEDataCollector"];
